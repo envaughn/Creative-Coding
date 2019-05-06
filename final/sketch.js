8b440b55d83fe89687a7fe6d8d1efc1dc45bace5
envaughn <47069023+envaughn@users.noreply.github.com>
2019-05-05 18:36:11 -0600

    added comments & removed deactivated code

--- final/sketch.js
+++ final/sketch.js
@@ -1,8 +1,11 @@
-var livRoom, title, kitchRoom, textOn, goodBye, bg, theTxt;
+var livRoom, textOn, bg, theTxt;
 let items = [];
 let pics = [];
 let ctxt = [];
+//declaring arrays for image and text, and the Item class
 
+
+//values for the text box, declared here so it could move while I was testing.
 let tB = {};
 tB.x = 660;
 tB.y = 453;
@@ -13,62 +16,52 @@ tB.h = 270;
 function preload() {
 bg = loadImage('stuff/bg/newtestbg.gif');
 
+//preloading the text and images
+//the text was going to be strings
+
   for (let i = 0; i < 3; i++) {
     pics[i] = loadImage(`stuff/obj/item`+ i +`.gif`);
     for (let o = 0; o < 3; o++) {
       ctxt[o] = loadImage(`stuff/text/txt`+ o +`.gif`)
     }
-    // ctxt[1] = loadImage(`stuff/obj/text0.gif`);
-
   }
-
-
-    //console.log("pic width: " + pics.width);
-    //console.log("2: " + pics.length);
   }
-
-  // for (let i = 0; i < 2; i++) {
-  //
-  // }
 
 
 
 function setup() {
 
-var x, y, c, w, h, px, py, textOn, theTxt;
+var x, y, c, w, h, px, py, textOn;
 
 textAlign(CENTER, TOP)
 rectMode(CENTER)
 textSize(15);
   createCanvas(800, 600);
 
-
-
-   items[0] = new Item(300, 0, 502, 0);
+//The interactive items, a for loop wasn't possible here because of the locations being fed to the constructor.
+   items[0] = new Item(400, 100, 502, 0, 200);
    items[1] = new Item(380, 340, 32, 1);
    items[2] = new Item(350, 360, 250, 2);
-   items[3] = new Item(500, 100, 100, 3);
-   items[4] = new Item(750, 500, 25, 4);
+
+
+//The last value is how I decided to tie the image to the Item class, so
+// item[1] corresponds to pics[1]
+
 livRoom = true
-goodBye = false
-
-
 
 }
 
 function draw() {
-
     background(bg);
     noStroke();
-
 advance();
-
-
 
 }
 
-
 function advance() {
+
+  //created a possibility for more "rooms" with different items
+  //    by creating true/false on looping of all the objects
 
   if (livRoom) {
     for (let i = 0; i < 4; i++)  {
@@ -81,22 +74,17 @@ function advance() {
     }
   } else {
     console.log('texton null')
+    background(bg)
   }
-    // console.log('helo?')
-
-
-// } else { if (testRm)
-//   console.log('livRoom null')
-// } else
-//
-//   }
+//a previous version of this worked, but because the text boxes are all in
+// the same location I couldn't figure out how to make the correct text
+// to loop with the right object
 }
-
 
     function mousePressed() {
      for (let allitems of items) {
         allitems.textEv(mouseX, mouseY, theTxt);
-
+//allows text to show
 
 }
     }
@@ -107,8 +95,7 @@ class Item {
     this.y = y;
     this.w = w;
     this.h = h;
-    this.c = c;
-
+    this.c = c; //the value that chooses the image/text
   }
 
   imgShow () {
@@ -125,28 +112,12 @@ txtShow (t) {
 }
 
 textEv (px, py) {
-if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.w) {
+if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
+console.log('itemclick' + this.c)
 textOn = true;
-theTxt = this.c
+theTxt = this.c // <- I'm pretty sure this is the issue.
 
 }
 }
 
 }
-
-
-
-  //  console.log("hey");
-    //textOn = true;
-
- // } else if (px )
-
-    // textOpen = true
-    // livRoom = true
-
-//   textDis (px, py, d) {
-//     d = dist(px, py, tB.x, tB.y )
-//     if (!textOpen ){
-//       if (d > tB.h) {
-//     }
-// }
