8608dfcd2635c36c727929b3bfc2fbebcde0eff8
envaughn <47069023+envaughn@users.noreply.github.com>
2019-05-06 00:05:40 -0600

    Update sketch.js

--- final/sketch.js
+++ final/sketch.js
@@ -15,7 +15,7 @@ tB.h = 270;
 
 function preload() {
 bg = loadImage('stuff/bg/newtestbg.gif');
-
+theText = 2
 //preloading the text and images
 //the text was going to be strings
 
@@ -33,15 +33,16 @@ function setup() {
 
 var x, y, c, w, h, px, py, textOn;
 
+background(bg)
 textAlign(CENTER, TOP)
 rectMode(CENTER)
 textSize(15);
   createCanvas(800, 600);
-
+  background(bg);
 //The interactive items, a for loop wasn't possible here because of the locations being fed to the constructor.
    items[0] = new Item(400, 100, 502, 0, 200);
    items[1] = new Item(380, 340, 32, 1);
-   items[2] = new Item(350, 360, 250, 2);
+   items[2] = new Item(330, 365, 250, 2);
 
 
 //The last value is how I decided to tie the image to the Item class, so
@@ -52,7 +53,6 @@ livRoom = true
 }
 
 function draw() {
-    background(bg);
     noStroke();
 advance();
 
@@ -64,18 +64,22 @@ function advance() {
   //    by creating true/false on looping of all the objects
 
   if (livRoom) {
-    for (let i = 0; i < 4; i++)  {
+    for (let i = 0; i < 3; i++)  {
       items[i].imgShow();
     }
-  }
-    if (textOn) {
-  for (let i = 0; i < 4; i++)  {
-        items[i].imgShow(theTxt);
-    }
   } else {
-    console.log('texton null')
-    background(bg)
+
   }
+
+  if (textOn) {
+  for (let o = 0; o < 3; o++)  {
+        items[o].txtShow(theText);
+  }
+} else {
+    console.log('texton null')
+
+  }
+//  noLoop ();
 //a previous version of this worked, but because the text boxes are all in
 // the same location I couldn't figure out how to make the correct text
 // to loop with the right object
@@ -83,7 +87,7 @@ function advance() {
 
     function mousePressed() {
      for (let allitems of items) {
-        allitems.textEv(mouseX, mouseY, theTxt);
+        allitems.textEv(mouseX, mouseY);
 //allows text to show
 
 }
@@ -105,9 +109,8 @@ class Item {
 
 }
 txtShow (t) {
-  t = theTxt
   if (textOn) {
-  image(ctxt[1], tB.x - (tB.x/4), tB.y - (tB.y/4), tB.w, tB.h);
+  image(ctxt[t], tB.x - (tB.x/4), tB.y - (tB.y/4), tB.w, tB.h);
 }
 }
 
@@ -115,7 +118,9 @@ textEv (px, py) {
 if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
 console.log('itemclick' + this.c)
 textOn = true;
-theTxt = this.c // <- I'm pretty sure this is the issue.
+
+//^^^ this is the point where it works, when you click on the item it gives the right c value.
+
 
 }
 }
